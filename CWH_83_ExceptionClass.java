54132dc0d93bd1f73b96ae33549b81fac826019f
Pragyesh Kumar Seth <116973800+pragyesh7753@users.noreply.github.com>
2024-10-21 23:12:37 +0530

    21/10/2024

--- CWH_83_ExceptionClass.java
+++ CWH_83_ExceptionClass.java
@@ -5,14 +5,28 @@ class MyException extends Exception {
 
     @Override
     public String toString() {
+        // return super.toString() + " I am toString()";
         return super.toString() + " I am toString()";
     }
 
     @Override
     public String getMessage() {
+        // return super.getMessage() + " I am getMessage()";
         return super.getMessage() + " I am getMessage()";
     }
 
+}
+
+class MaxAgeException extends Exception {
+    @Override
+    public String toString() {
+        return "Age cannot be greater than 125";
+    }
+
+    @Override
+    public String getMessage() {
+        return "Make sure that the value of age entered is correct";
+    }
 }
 
 class CWH_83_ExceptionClass {
@@ -24,11 +38,15 @@ class CWH_83_ExceptionClass {
         try {
             if (a < 99) {
                 throw new MyException();
+                // throw new ArithmeticException("This is an exception");
             }
         } catch (MyException e) {
             System.out.println(e.getMessage());
             System.out.println(e.toString());
-            System.out.println(e);
+            // System.out.println(e);
+            e.printStackTrace();
+            System.out.println("Finished");
         }
+        System.out.println("Yes Finished");
     }
 }
