080e2a17d935b00cc0e7ae5be54ff9b38ae2ffd2
Pragyesh Kumar Seth <spragyesh86@gmail.com>
2024-11-29 13:01:23 +0530

    29/11/2024

---
+++ CWH_94_Hashing.java
@@ -0,0 +1,13 @@
+// Hashing is a technique to convert a range of key values into a range of indexes of an array.
+// Situation of hash collision: When two keys have the same hash value.
+// Open Addressing: When a collision occurs, we find the next empty slot in the array.
+// Chaining: When a collision occurs, we store the keys in the same index of the array in the form of a linked list.
+// Load Factor: The ratio of the number of keys to the number of slots in the array.
+// Rehashing: When the load factor exceeds a certain threshold, we increase the size of the array and rehash all the keys.
+// Java provides a HashTable class that implements the Map interface.
+// Classes that implement the Map interface: HashSet, HashMap, LinkedHashMap, TreeMap, HashTable. These are all part of Collections Framework.
+public class CWH_94_Hashing {
+    public static void main(String[] args) {
+
+    }
+}
